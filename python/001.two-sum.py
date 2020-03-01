8c176a9479e685c51b71fef6f8a81fd81118692b
悟空 <25031515+haowukong@users.noreply.github.com>
2020-03-01 11:37:08 +0800

    Update 001.two-sum.py

--- python/001.two-sum.py
+++ python/001.two-sum.py
@@ -1,11 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        n=len(nums)
-        if n<2:
+        if len(nums)<2:
             return None
         map={}
         for i,n in enumerate(nums):
             if target-n in map:
                 return [i,map[target-n]]
-            else:
-                map[n]=i
+            map[n]=i
