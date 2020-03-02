6a7e96de247d4ca34b917de8df5a62769ab8ceff
悟空 <25031515+haowukong@users.noreply.github.com>
2020-03-02 21:26:27 +0800

    Update 027. Remove Element.cpp

--- C++/027. Remove Element.cpp
+++ C++/027. Remove Element.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
         int len=0;
-        for(int i=0;i<nums.size();++i){
-            if(nums[i]!=val) nums[len++]=nums[i];
-        }
+        for(int i=0;i<nums.size();++i)
+            if(nums[i]!=val)
+                nums[len++]=nums[i];
         return len;
     }
 };
