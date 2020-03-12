1a55632e245cc4641befbfb2b1a8530242aa1257
悟空 <25031515+haowukong@users.noreply.github.com>
2020-03-12 21:41:51 +0800

    Create 023.merge-k-sorted-lists.cpp

---
+++ C++/023.merge-k-sorted-lists.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size()==0)
+            return NULL;
+        vector<int> num;
+        for(int i=0;i<lists.size();++i){
+            ListNode *p=lists[i];
+            while(p!=NULL){
+                num.push_back(p->val);
+                p=p->next;
+            }
+        }
+        if(num.size()==0)
+            return NULL;
+        sort(num.begin(),num.end());
+        ListNode*l1,*l2=NULL;
+        for(int i=num.size()-1;i>=0;i--)
+        {
+            l1=new ListNode(num[i]);
+            l1->next=l2;
+            l2=l1;
+        }
+        return l1;
+    }
+};
