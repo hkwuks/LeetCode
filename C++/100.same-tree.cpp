c1d13e959c0779d7401f954f7480a05d3cb13b1f
悟空 <25031515+haowukong@users.noreply.github.com>
2020-03-19 16:38:33 +0800

    Update 100.same-tree.cpp

--- C++/100.same-tree.cpp
+++ C++/100.same-tree.cpp
@@ -18,3 +18,12 @@ public:
             return false;
     }
 };
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p != NULL && q != NULL)
+            return p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        return p == NULL && q == NULL;
+    }
+};
