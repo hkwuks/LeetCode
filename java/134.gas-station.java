2fe9199c7138489606eca376fb5b8abd09f19945
悟空 <25031515+haowukong@users.noreply.github.com>
2020-03-29 22:13:53 +0800

    Create 134.gas-station.java

---
+++ java/134.gas-station.java
@@ -0,0 +1,19 @@
+class Solution{
+    public int canCompleteCircuit(int[] gas,int[] cost){
+        int n=gas.length;
+
+        int total_tank=0;
+        int curr_tank=0;
+        int starting_station=0;
+
+        for(int i=0;i<n;++i){
+            total_tank+=gas[i]-cost[i];
+            curr_tank+=gas[i]-cost[i];
+            if(curr_tank<0){
+                starting_station=i+1;
+                curr_tank=0;
+            }
+        }
+        return total_tank>=0?starting_station:-1;
+    }
+}
