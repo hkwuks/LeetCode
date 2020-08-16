7a2b54b30f5618ee96520ab8b59e1fa6d2ce749c
hkwuks <25031515+hkwuks@users.noreply.github.com>
2020-08-16 13:04:02 +0800

    更新了注释
    主要是接口的注释，还有部分自己写的提示

--- C++/101.symmetric-tree.cpp
+++ C++/101.symmetric-tree.cpp
@@ -4,7 +4,9 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
@@ -19,7 +21,7 @@ private:
             return t1->val==t2->val&&isMirror(t1->right,t2->left)&&isMirror(t1->left,t2->right);
         return t1==NULL&&t2==NULL;
     }
-
+//第二种
 private:
     bool isMirror(TreeNode* t1,TreeNode* t2){
         if(t1==NULL&&t2==NULL)return true;
