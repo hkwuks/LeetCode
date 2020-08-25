9c11fcf6d95b204a9b9ae396824a8a35284b294d
hkwuks <25031515+hkwuks@users.noreply.github.com>
2020-08-25 23:40:48 +0800

    Create 242.valid-anagram.cpp

---
+++ C++/242.valid-anagram.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()==0&&t.size()==0) return true;
+        if(s.size()!=t.size()) return false;
+        unordered_map<char,int> count;
+        for(int i=0;i<s.size();++i){
+            ++count[s[i]];
+            --count[t[i]];
+        }
+        for(int j=0;j<s.size();++j){
+            if(count[s[j]]!=0)
+                return false;
+        }
+        return true;
+    }
+};
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()==0&&t.size()==0) return true;
+        if(s.size()!=t.size()) return false;
+        unordered_map<char,int> count;
+        for(char i:s){
+            ++count[i];
+        };
+        for(char j:t){
+            --count[j];
+            if(count[j]<0)
+                return false;
+        }
+        return true;
+    }
+};
