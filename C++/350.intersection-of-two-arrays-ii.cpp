37fca17ef475cc2f60fb20f6d6722e7db77ef98d
hkwuks <25031515+hkwuks@users.noreply.github.com>
2020-08-25 23:40:25 +0800

    Create 350.intersection-of-two-arrays-ii.cpp

---
+++ C++/350.intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        if (nums1.size() > nums2.size())
+            return intersect(nums2, nums1);
+
+        unordered_map<int, int> map;
+        vector<int> ans;
+        for (int num : nums1) {
+            ++map[num];
+        }
+        for (int num : nums2) {
+            if (map[num] > 0) {
+                --map[num];
+                ans.emplace_back(num);
+            }
+        }
+        return ans;
+    }
+};
