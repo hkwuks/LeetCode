a4398e530c97a38418e27837188e8c7a92e68d1b
hkwuks <25031515+hkwuks@users.noreply.github.com>
2020-08-25 23:40:39 +0800

    Create 290.word-pattern.cpp

---
+++ C++/290.word-pattern.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string str) {
+        unordered_map<char, string> map;
+        unordered_map<string, char> rmap;
+        stringstream ss(str);string s;
+        for (char c : pattern) {
+            if (!(ss >> s) || (map.count(c) && map[c] != s) || (rmap.count(s) && rmap[s] != c)) return false;
+            map[c] = s;rmap[s] = c;
+        }
+        return (ss >> s) ? false : true;
+    }
+};
