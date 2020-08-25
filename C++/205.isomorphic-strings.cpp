b654cdac9ec42ae798d2c3a6ef0d0da04e556c93
hkwuks <25031515+hkwuks@users.noreply.github.com>
2020-08-25 18:00:27 +0800

    Create 205.isomorphic-strings.cpp

---
+++ C++/205.isomorphic-strings.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if (s.size() == 0 && t.size() == 0) return true;
+        if (s.size() != t.size()) return false;
+        for (int i = 0;i < s.size();++i)
+            if (s.find(s[i]) != t.find(t[i]))
+                return false;
+        return true;
+    }
+};
