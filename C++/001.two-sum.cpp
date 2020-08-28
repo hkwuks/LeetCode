c6bf84b016fa810a84ab7d1236a942bd27f94696
hkwuks <25031515+hkwuks@users.noreply.github.com>
2020-08-28 22:35:05 +0800

    Create 001.two-sum.cpp

---
+++ C++/001.two-sum.cpp
@@ -0,0 +1,18 @@
+//一遍哈希表，时间复杂度N
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> map;
+        vector<int> v(2);
+        for (int i = 0;i < nums.size();++i) {
+            int complement = target - nums[i];
+            if (map.count(complement)) {
+                v[0] = map[complement];
+                v[1] = i;
+                break;
+            }
+            map[nums[i]] = i;
+        }
+        return v;
+    }
+};
