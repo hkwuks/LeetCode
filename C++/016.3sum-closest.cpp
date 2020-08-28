c4c74b85d0aed5270466582c612acadae3313c25
hkwuks <25031515+hkwuks@users.noreply.github.com>
2020-08-28 22:35:21 +0800

    Update 016.3sum-closest.cpp

--- C++/016.3sum-closest.cpp
+++ C++/016.3sum-closest.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end());
-        int ans=nums[0]+nums[1]+nums[2];
-        for(int i=0;i<nums.size()-2;++i){
-            int L=i+1,R=nums.size()-1;
-            while(L<R){
-                int sum=nums[i]+nums[L]+nums[R];
-                if(abs(target-sum)<abs(target-ans))
-                    ans=sum;
-                if(sum>target)--R;
-                else if(sum<target)++L;
-                else return ans;
+        sort(nums.begin(), nums.end());
+        int ans = nums[0] + nums[1] + nums[2];
+        for (int i = 0;i < nums.size() - 2;++i) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int L = i + 1, R = nums.size() - 1;
+            while (L < R) {
+                int tmp = nums[i] + nums[L] + nums[R];
+                if (abs(tmp - target) < abs(ans - target))
+                    ans = tmp;
+                if (tmp < target) ++L;
+                else if (tmp > target) --R;
+                else return target;
             }
         }
         return ans;
