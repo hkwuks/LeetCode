df97a1612d469840aad366f93c4301c15c9045b6
hkwuks <25031515+hkwuks@users.noreply.github.com>
2020-09-11 22:04:24 +0800

    Update 242.valid-anagram.cpp

--- C++/242.valid-anagram.cpp
+++ C++/242.valid-anagram.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size()==0&&t.size()==0) return true;
-        if(s.size()!=t.size()) return false;
-        unordered_map<char,int> count;
-        for(int i=0;i<s.size();++i){
+        if (s.size() == 0 && t.size() == 0) return true;
+        if (s.size() != t.size()) return false;
+        unordered_map<char, int> count;
+        for (int i = 0;i < s.size();++i) {
             ++count[s[i]];
             --count[t[i]];
         }
-        for(int j=0;j<s.size();++j){
-            if(count[s[j]]!=0)
+        for (int j = 0;j < s.size();++j) {
+            if (count[s[j]] != 0)
                 return false;
         }
         return true;
@@ -19,15 +19,15 @@ public:
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size()==0&&t.size()==0) return true;
-        if(s.size()!=t.size()) return false;
-        unordered_map<char,int> count;
-        for(char i:s){
+        if (s.size() == 0 && t.size() == 0) return true;
+        if (s.size() != t.size()) return false;
+        unordered_map<char, int> count;
+        for (const char& i : s) {
             ++count[i];
         };
-        for(char j:t){
+        for (const char& j : t) {
             --count[j];
-            if(count[j]<0)
+            if (count[j] < 0)
                 return false;
         }
         return true;
