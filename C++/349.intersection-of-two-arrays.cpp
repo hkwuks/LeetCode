97e257d82ccd035e4fb6efef32c78de33828f76e
hkwuks <25031515+hkwuks@users.noreply.github.com>
2020-09-11 22:02:13 +0800

    Update 349.intersection-of-two-arrays.cpp

--- C++/349.intersection-of-two-arrays.cpp
+++ C++/349.intersection-of-two-arrays.cpp
@@ -4,19 +4,19 @@ public:
         unordered_set<int> set1;
         unordered_set<int> set2;
         vector<int> ans;
-        for (int num : nums1) {
+        for (const int& num : nums1) {
             set1.emplace(num);
         }
-        for (int num : nums2) {
+        for (const int& num : nums2) {
             set2.emplace(num);
         }
         if (set1.size() < set2.size()) {
-            for (int num : set1)
+            for (const int& num : set1)
                 if (set2.count(num) == 1)
                     ans.emplace_back(num);
         }
         else {
-            for (int num : set2)
+            for (const int& num : set2)
                 if (set1.count(num) == 1)
                     ans.emplace_back(num);
         }
