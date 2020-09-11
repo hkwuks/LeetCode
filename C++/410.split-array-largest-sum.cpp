47d3de65b92a0c1362459cdfca416beb1dc56c92
hkwuks <25031515+hkwuks@users.noreply.github.com>
2020-09-11 22:01:54 +0800

    Update 410.split-array-largest-sum.cpp

--- C++/410.split-array-largest-sum.cpp
+++ C++/410.split-array-largest-sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int splitArray(vector<int>& nums, int m) {
         long long left = 0, right = 0;
-        for (int num : nums) {
+        for (const int& num : nums) {
             right += num;
             if (left < num)
                 left = num;
@@ -21,7 +21,7 @@ public:
     bool check(vector<int>& nums, int x, int m) {
         long long sum = 0;
         int cnt = 1;
-        for (int num : nums) {
+        for (const int& num : nums) {
             if (sum + num > x) {
                 ++cnt;
                 sum = num;
