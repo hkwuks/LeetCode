d670b7dd92d9b1083fb735102d6f831d96b4e4cc
hkwuks <25031515+hkwuks@users.noreply.github.com>
2020-12-14 22:47:20 +0800

    Update 049.group-anagrams.java

--- java/049.group-anagrams.java
+++ java/049.group-anagrams.java
@@ -1,16 +1,27 @@
 //排序
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String,List<String>> map=new HashMap<String,List<String>>();
-        for(String str:strs){
-            char[] array=str.toCharArray();
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        for (String str : strs) {
+            char[] array = str.toCharArray();
             Arrays.sort(array);
-            String key=new String(array);
-            List<String> list=map.getOrDefault(key,new ArrayList<String>()); //getOrDefault():如果map中存在key，就使用key，否则使用Default Value
+            String key = new String(array);
+            List<String> list = map.getOrDefault(key, new ArrayList<String>()); //getOrDefault():如果map中存在key，就使用key，否则使用Default Value
             list.add(str);
-            map.put(key,list);
+            map.put(key, list);
         }
         return new ArrayList<List<String>>(map.values());
+    }
+}
+
+//排序（炫技）
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        return new ArrayList<>(Arrays.stream(strs).collect(Collectors.groupingBy(str -> { // collect:收集器；Collector是collect方法的参数；Collectors是Collector的实现
+            char[] array = str.toCharArray();
+            Arrays.sort(array);
+            return new String(array);
+        })).values());
     }
 }
 
