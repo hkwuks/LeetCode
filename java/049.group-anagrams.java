391da63f4c639d51f230a8acc2f1c155adaa5203
hkwuks <25031515+hkwuks@users.noreply.github.com>
2020-12-14 22:03:43 +0800

    Create 049.group-anagrams.java

---
+++ java/049.group-anagrams.java
@@ -0,0 +1,41 @@
+//排序
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List<String>> map=new HashMap<String,List<String>>();
+        for(String str:strs){
+            char[] array=str.toCharArray();
+            Arrays.sort(array);
+            String key=new String(array);
+            List<String> list=map.getOrDefault(key,new ArrayList<String>()); //getOrDefault():如果map中存在key，就使用key，否则使用Default Value
+            list.add(str);
+            map.put(key,list);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+}
+
+//计数
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        for (String str : strs) {
+            int[] counts = new int[26]; 
+            int length=str.length();
+            for (int i = 0; i < 26; ++i) {
+                ++counts[str.charAt(i) - 'a'];
+            }
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < 26; ++i) {
+                if (counts[i] != 0) {
+                    sb.append((char) ('a' + i));
+                    sb.append(counts[i]);
+                }
+            }
+            String key = sb.toString();
+            List<String> list=map.getOrDefault(key,new ArrayList<String>())l
+            list.add(str);
+            map.put(key,list);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+}
