e802e4d5260ed281a6fcebdd25597a6581b67053
hkwuks <25031515+hkwuks@users.noreply.github.com>
2020-12-20 18:42:52 +0800

    Update 001.two-sum.cpp

--- C++/001.two-sum.cpp
+++ C++/001.two-sum.cpp
@@ -16,3 +16,17 @@ public:
         return v;
     }
 };
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        if (nums.size() < 2)return {};
+        map<int, int> m;
+        for (int i = 0;i < nums.size();++i) {
+            if (m.find(target - nums[i]) != m.end())
+                return { m[target - nums[i]],i };
+            m[nums[i]] = i;
+        }
+        return {};
+    }
+};
