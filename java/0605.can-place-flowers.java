9a8ed48dfd75757b69b582ff4464778b43132602
hkwuks <25031515+hkwuks@users.noreply.github.com>
2021-01-01 15:07:50 +0800

    Create 0605.can-place-flowers.java

---
+++ java/0605.can-place-flowers.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed,int n) {
+        int len = flowerbed.length;
+        int count = 0;
+        int prev = -1;
+        for (int i = 0; i < len; ++i) {
+            if (flowerbed[i] == 1) {
+                if (prev < 0) {
+                    count += i / 2;
+                } else {
+                    count += (i - prev - 2) / 2;
+                }
+                if (count >= n) {
+                    return true;
+                }
+                prev = i;
+            }
+        }
+        if (prev < 0) {
+            count += (len + 1) / 2;
+        } else {
+            count += (len - prev - 1) / 2;
+        }
+        return count >= n;
+    }
+}
