ce2cc4df9a4d92240e9fb77ee8cf879c4e946fa0
hkwuks <1013302315@qq.com>
2021-07-18 19:16:20 +0800

    Update 0139.word-break.java

--- java/0139.word-break.java
+++ java/0139.word-break.java
@@ -6,7 +6,31 @@ class Solution {
         dp[0] = true;
         for (int i = 1; i <= s.length(); ++i) {
             for (int j = 0; j < i; ++j) {
-                if (dp[j] && wordDict.contains(s.substring(j, i))) {
+                if (dp[j] && wordDictSet.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}
+
+
+// 优化
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashSet wordDictSet=new HashSet();
+        int maxLen = 0;
+        for (String word : wordDict) {
+            wordDictSet.add(word);
+            maxLen = Math.max(maxLen, word.length());
+        }
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); ++i) {
+            for (int j = i; j >= i - maxLen && j >= 0; --j) {
+                if (dp[j] && wordDictSet.contains(s.substring(j, i))) {
                     dp[i] = true;
                     break;
                 }
