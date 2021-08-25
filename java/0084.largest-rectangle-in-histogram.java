46281133d86cae83313a52ef729350e8af76dd81
hkwuks <1013302315@qq.com>
2021-08-26 00:10:21 +0800

    Create 0084.largest-rectangle-in-histogram.java

---
+++ java/0084.largest-rectangle-in-histogram.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int len = heights.length;
+        int[] left = new int[len];
+        int[] right = new int[len];
+        Stack<Integer> stack = new Stack<Integer>();
+        for (int i = 0; i < len; ++i) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+                stack.pop();
+            }
+            left[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(i);
+        }
+        stack.clear();
+        for (int i = len - 1; i >= 0; --i) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+                stack.pop();
+            }
+            right[i] = stack.isEmpty() ? len : stack.peek();
+            stack.push(i);
+        }
+        int ans = 0;
+        for (int i = 0; i < len; ++i) {
+            ans = Math.max(ans, (right[i] - left[i] - 1) * heights[i]);
+        }
+        return ans;
+    }
+}
+
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int len = heights.length;
+        int[] left = new int[len];
+        int[] right = new int[len];
+        Arrays.fill(right, len);
+
+        Stack<Integer > stack = new Stack <Integer >();
+        for (int i = 0; i < len; ++i) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+                right[stack.peek()] = i;
+                stack.pop();
+            }
+            left[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(i);
+        }
+        int ans = 0;
+        for (int i = 0; i < len; ++i) {
+            ans = Math.max(ans, (right[i] - left[i] - 1) * heights[i]);
+        }
+        return ans;
+    }
+}
