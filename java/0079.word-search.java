8bf173b23a211c2221823c2265e1301ae4be3378
hkwuks <1013302315@qq.com>
2021-08-25 16:34:43 +0800

    Create 0079.word-search.java

---
+++ java/0079.word-search.java
@@ -0,0 +1,48 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int h = board.length, w = board[0].length,wlen=word.length();
+        boolean[][] visited = new boolean[h][w];
+        boolean[][][] isOk = new boolean[h][w][wlen];
+        for (int i = 0; i < h; ++i) {
+            for (int j = 0; j < w; ++j) {
+                for (int k = 0; k < wlen; ++k) {
+                    isOk[i][j][k] = true;
+                }
+            }
+        }
+        for (int i = 0; i < h; ++i) {
+            for (int j = 0; j < w; ++j) {
+                if (check(board, visited, isOk, i, j, word, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean check(char[][] board, boolean[][] visited, boolean[][][] isOk, int i, int j, String s, int k) {
+
+        if (board[i][j] != s.charAt(k)) {
+            isOk[i][j][k] = false;
+            return false;
+        }
+        if (k == s.length() - 1) {
+            return true;
+        }
+        if (isOk[i][j][k]==false) {
+            return false;
+        }
+        visited[i][j] = true;
+        int[][] directions = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
+        boolean result = false;
+        for (int[] dir : directions) {
+            int newi = i + dir[0], newj = j + dir[1];
+            if (newi >= 0 && newi < board.length && newj >= 0 && newj < board[0].length && !visited[newi][newj]) {
+                if(check(board, visited, isOk, newi, newj, s, k + 1))
+                result = true;
+            }
+        }
+        visited[i][j] = false;
+        return result;
+    }
+}
