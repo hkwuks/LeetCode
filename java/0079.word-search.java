26ff03a469c074f8c5c043e5993197baec60ad97
hkwuks <1013302315@qq.com>
2021-08-27 17:49:17 +0800

    Update 0079.word-search.java

--- java/0079.word-search.java
+++ java/0079.word-search.java
@@ -1,36 +1,33 @@
-class Solution {
+public class Solution {
     public boolean exist(char[][] board, String word) {
-        int h = board.length, w = board[0].length;
-        boolean[][] visited = new boolean[h][w];
-        for (int i = 0; i < h; ++i) {
-            for (int j = 0; j < w; ++j) {
-                if (check(board, visited, i, j, word, 0)) {
+        int h = board.length, w = board[0].length,wlen=word.length();
+        boolean[][] visited=new boolean[h][w];
+        for(int i=0;i<h;++i){
+            for(int j=0;j<w;++j){
+                if(check(board,word,i,j,visited,0)){
                     return true;
                 }
             }
         }
         return false;
     }
-
-    private boolean check(char[][] board, boolean[][] visited, int i, int j, String s, int k) {
-
-        if (board[i][j] != s.charAt(k)) {
+    private boolean check(char[][] board,String word,int i,int j,boolean[][] visited,int k){
+        if(word.charAt(k)!=board[i][j]){
             return false;
         }
-        if (k == s.length() - 1) {
+        if(k==word.length()-1){
             return true;
         }
-        visited[i][j] = true;
-        int[][] directions = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
-        boolean result = false;
-        for (int[] dir : directions) {
-            int newi = i + dir[0], newj = j + dir[1];
-            if (newi >= 0 && newi < board.length && newj >= 0 && newj < board[0].length && !visited[newi][newj]) {
-                if(check(board, visited, newi, newj, s, k + 1))
-                result = true;
+        visited[i][j]=true;
+        int[][] directions={{1,0},{-1,0},{0,1},{0,-1}};
+        boolean ans=false;
+        for(int[] dir:directions){
+            int ni=i+dir[0],nj=j+dir[1];
+            if(ni>=0&&ni<board.length&&nj>=0&&nj<board[0].length&&!visited[ni][nj] && check(board,word,ni,nj,visited,k+1)){
+                return true;
             }
         }
-        visited[i][j] = false;
-        return result;
+        visited[i][j]=false;
+        return false;
     }
 }
