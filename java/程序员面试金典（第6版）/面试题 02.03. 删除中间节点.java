c972a82659fe606d9f138584671439b2739b306c
hkwuks <1013302315@qq.com>
2021-11-25 15:54:10 +0800

    Create 面试题 02.03. 删除中间节点.java

---
+++ java/程序员面试金典（第6版）/面试题 02.03. 删除中间节点.java
@@ -0,0 +1,15 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        ListNode q = node;
+        q.val = q.next.val;
+        q.next = q.next.next;
+    }
+}
