aaa1f66c6c5f607668305a790e14e59ec0f62a1c
hkwuks <25031515+hkwuks@users.noreply.github.com>
2025-12-07 19:41:40 +0800

    Update 0169. Majority Element.py

--- python/0169. Majority Element.py
+++ python/0169. Majority Element.py
@@ -16,3 +16,14 @@ class Solution:
         counts = collections.Counter(nums)
         return max(counts.keys(), key=counts.get)
 
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        cand = None
+
+        for n in nums:
+            if count == 0:
+                cand = n
+            count +=(1 if n==cand else -1)
+
+        return cand
