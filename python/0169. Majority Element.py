046043149096130e6d2a8394c731f1cc798ff418
hkwuks <25031515+hkwuks@users.noreply.github.com>
2025-12-07 19:03:18 +0800

    Create 0169. Majority Element.py

---
+++ python/0169. Majority Element.py
@@ -0,0 +1,18 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        mid_count = len(nums)//2
+        while True:
+            cand = random.choice(nums)
+            if sum(1 for e in nums if e==cand) > mid_count:
+                return cand
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        nums.sort()
+        return nums[len(nums)//2]
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        counts = collections.Counter(nums)
+        return max(counts.keys(), key=counts.get)
+
