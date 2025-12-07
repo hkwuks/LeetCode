7bc7f18032bda7f8ce379188460a46c892f143d8
hkwuks <25031515+hkwuks@users.noreply.github.com>
2025-12-07 12:38:13 +0800

    Update 0274. H-Index.py

--- python/0274. H-Index.py
+++ python/0274. H-Index.py
@@ -34,3 +34,50 @@ class Solution:
         if cnt < h:
             return False
         return True
+
+
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        l,r=0,len(citations)
+        while l<r:
+            mid = (l+r+1)>>1
+            cnt=0
+
+            for v in citations:
+                if v>=mid:
+                    cnt+=1
+            
+            if cnt>=mid:
+                l = mid
+            else:
+                r = mid-1
+        
+        return l
+
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        n = len(citations)
+        tot=0
+        counter = [0] * (n+1)
+        for c in citations:
+            if c>=n:
+                counter[n]+=1
+            else:
+                counter[c]+=1
+        
+        for i in range(n,-1,-1):
+            tot+=counter[i]
+            if tot>=i:
+                return i
+        return 0
+
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        sorted_c = sorted(citations,reverse=True)
+        i=0
+        h=0
+        n=len(citations)
+        while i<n and sorted_c[i]>h:
+            h+=1
+            i+=1
+        return h
